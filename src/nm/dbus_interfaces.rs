24b9a4ad131e7ba66ba4ca2c82ef1747c9fe273e
Aashish Thapa <aashishthapa520@gmail.com>
2025-12-24 19:03:24 -0600

    feat(nm): add D-Bus interfaces placeholder

---
+++ src/nm/dbus_interfaces.rs
@@ -0,0 +1,5 @@
+// NetworkManager D-Bus interface definitions
+// This file can be extended with zbus proxy definitions for type-safe D-Bus calls
+
+// For now, we use dynamic Proxy calls in mod.rs
+// Future: Could use #[proxy] macro for compile-time checked interfaces
